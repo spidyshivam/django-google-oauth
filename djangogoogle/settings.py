827c99a545d5ff6f971bb5f24ec8fdc5ee33a894
Shivam Patel <shivampatel887@gmail.com>
2025-03-01 14:25:47 +0530

    *

--- djangogoogle/settings.py
+++ djangogoogle/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-g(v!j6potax-@e(q@pbru$hc5wq!r3g=@2825_0n$eheli!q(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

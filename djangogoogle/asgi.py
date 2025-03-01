f3837f45d7118160e067c177d78eedc4dbf08887
Shivam Patel <shivampatel887@gmail.com>
2025-03-01 15:47:01 +0530

    fixing

--- djangogoogle/asgi.py
+++ djangogoogle/asgi.py
@@ -6,16 +6,17 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogoogle.settings')
+django.setup()  # Add this line if missing
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogoogle.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
